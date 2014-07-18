88042e30270acb279551216654ad9b4666d2aa59
Ivan Fantom <ziv78@yandex.ru>
2014-07-18 16:33:09 +0300

    code refactoring

---
+++ CommonTask/scripts/selector.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    $.extend($.expr[':'], {
+        inView: function (element) {
+            var $window = $(window);
+
+            var viewport = {
+                top: $window.scrollTop(),
+                left: $window.scrollLeft()
+            };
+            viewport.right = viewport.left + $window.width();
+            viewport.bottom = viewport.top + $window.height();
+            
+            var $element = $(element);
+            var offset = $element.offset();
+            offset.right = offset.left + $element.outerWidth();
+            offset.bottom = offset.top + $element.outerHeight();
+
+            return (viewport.right > offset.left &&
+                viewport.left < offset.right &&
+                viewport.bottom > offset.top &&
+                viewport.top < offset.bottom);
+        }
+    });
+})();
